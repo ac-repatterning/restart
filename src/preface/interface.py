01077363298b9dc3a2dc4828f40adc94ba2fe382
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-04 11:16:57 +0000

    Update src/preface/interface.py: In Progress

--- src/preface/interface.py
+++ src/preface/interface.py
@@ -1,8 +1,10 @@
 """Module interface.py"""
+import argparse
 import typing
 
 import boto3
 
+import config
 import src.elements.s3_parameters as s3p
 import src.elements.service as sr
 import src.functions.service
@@ -17,22 +19,36 @@ class Interface:
     """
 
     def __init__(self):
-        pass
-
-    @staticmethod
-    def __get_attributes(connector: boto3.session.Session) -> dict:
+        """
+        Constructor
         """
 
+        self.__configurations = config.Config()
+
+    def __get_attributes(self, connector: boto3.session.Session, args: argparse.Namespace) -> dict:
+        """
+
+        :param connector:
+        :param args:
         :return:
         """
 
-        key_name = 'data/restart/attributes.json'
+        # The baseline attributes
+        attributes = src.s3.configurations.Configurations(connector=connector).objects(
+            key_name=self.__configurations.attributes_key)
 
-        return src.s3.configurations.Configurations(connector=connector).objects(key_name=key_name)
+        # Codes
+        if args.codes is not None:
+            attributes['excerpt'] = args.codes
+        else:
+            attributes['excerpt'] = None
 
-    def exc(self) -> typing.Tuple[boto3.session.Session, s3p.S3Parameters, sr.Service, dict]:
+        return attributes
+
+    def exc(self, args: argparse.Namespace) -> typing.Tuple[boto3.session.Session, s3p.S3Parameters, sr.Service, dict]:
         """
 
+        :param args: Wherein -> codes: list[int] | None
         :return:
         """
 
@@ -40,7 +56,7 @@ class Interface:
         s3_parameters: s3p.S3Parameters = src.s3.s3_parameters.S3Parameters(connector=connector).exc()
         service: sr.Service = src.functions.service.Service(
             connector=connector, region_name=s3_parameters.region_name).exc()
-        attributes: dict = self.__get_attributes(connector=connector)
+        attributes: dict = self.__get_attributes(connector=connector, args=args)
 
         src.preface.setup.Setup(
             service=service, s3_parameters=s3_parameters).exc(reacquire=attributes['reacquire'])
