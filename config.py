a9ad0bde7a472d32ce9053dfe279c2bb449ba52f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-04 11:25:42 +0000

    Update config.py: In Progress

--- config.py
+++ config.py
@@ -24,4 +24,4 @@ class Config:
 
         # Template
         self.s3_parameters_key = 's3_parameters.yaml'
-        self.attributes_key = 'data/restart/attributes.json'
+        self.arguments_key = 'data/restart/arguments.json'
