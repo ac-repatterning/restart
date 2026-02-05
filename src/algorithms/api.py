8596e47cbbe7183c474200316e2e4857d9cff2b6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-05 23:19:33 +0000

    Update src/algorithms/api.py

--- src/algorithms/api.py
+++ src/algorithms/api.py
@@ -1,5 +1,6 @@
 """Module api.py"""
 import boto3
+import pandas as pd
 
 import src.algorithms.data
 
@@ -17,7 +18,25 @@ class API:
 
         self.__data = src.algorithms.data.Data(connector=connector)
 
-    def __call__(self, ts_id: int, starting: str, ending: str) -> dict | list[dict]:
+        # renaming
+        self.__rename = {'Timestamp': 'timestamp', 'Value': 'value', 'Quality Code': 'quality_code'}
+
+    def __get_frame(self, content: dict | list[dict]) -> pd.DataFrame:
+
+        # The data in data frame form
+        columns = content[0]['columns'].split(',')
+        frame = pd.DataFrame.from_records(data=content[0]['data'], columns=columns)
+        frame.rename(columns=self.__rename, inplace=True)
+
+        # The identification codes of the time series
+        frame = frame.assign(ts_id=content[0]['ts_id'])
+
+        # Group
+        frame['group'] = pd.to_datetime(frame['timestamp'], unit='ms').dt.year
+
+        return frame
+
+    def __call__(self, ts_id: int, starting: str, ending: str):
         """
         https://timeseries.sepa.org.uk/KiWIS/KiWIS?service=kisters&type=queryServices&datasource=0
         &request=getTimeseriesValues&ts_id=52438010
@@ -38,6 +57,7 @@ class API:
                '&md_returnfields=ts_id,ts_name,ts_unitname,ts_unitsymbol,station_id,'
                'catchment_id,parametertype_id,parametertype_name,river_name&dateformat=UNIX&format=json')
 
-        return self.__data.exc(
+        content: dict | list[dict] = self.__data.exc(
             url=url.format(ts_id=ts_id, starting=starting, ending=ending))
 
+        return self.__get_frame(content=content)
