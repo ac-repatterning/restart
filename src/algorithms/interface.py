cdca9c6bf09a9cd6c2e6e8ec0bb0f8c077aa57f1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-05 23:07:59 +0000

    Update src/algorithms/interface.py: In Progress

--- src/algorithms/interface.py
+++ src/algorithms/interface.py
@@ -1,6 +1,21 @@
 
+import dask
+import boto3
+
+import src.elements.partitions as prt
+import src.algorithms.api
+
 
 class Interface:
 
-    def __init__(self):
-        pass
+    def __init__(self, connector: boto3.session.Session):
+
+        self.__connector = connector
+
+    def exc(self, partitions: list[prt.Partitions]):
+
+        __api = dask.delayed(src.algorithms.api.API(connector=self.__connector).__call__)
+
+        computations = []
+        for partition in partitions:
+            data = __api(ts_id=partition.ts_id, starting=partition.starting, ending=partition.ending)
