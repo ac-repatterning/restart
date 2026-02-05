e47e2b76a655d946a9424c7beb03cf9f4cf9c002
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-05 23:48:47 +0000

    Update src/algorithms/persist.py: In Progress

--- src/algorithms/persist.py
+++ src/algorithms/persist.py
@@ -4,24 +4,42 @@ import os
 import pandas as pd
 
 class Persist:
+    """
+    
+    """
 
-    def __init__(self, blob: pd.DataFrame, catchment_id: int, ts_id: int):
+    def __init__(self, data: pd.DataFrame, catchment_id: int, ts_id: int):
+        """
 
-        self.__blob = blob
+        :param data:
+        :param catchment_id:
+        :param ts_id:
+        """
+
+        self.__data = data
 
         self.__path = os.path.join(os.getcwd(), str(catchment_id), str(ts_id))
         os.makedirs(self.__path)
 
     def __persist(self, group: int):
+        """
 
-        section: pd.DataFrame = self.__blob.loc[self.__blob['group'] == group, :]
+        :param group:
+        :return:
+        """
+
+        section: pd.DataFrame = self.__data.loc[self.__data['group'] == group, :]
         values = section.sort_values(by='timestamp', ignore_index=True)
         values.to_csv(os.path.join(self.__path, f'{group}-01-01.csv'), header=True, encoding='utf-8', index=False)
 
         return True
 
     def exc(self):
+        """
 
-        states = [self.__persist(group = group) for group in self.__blob['group'].unique()]
+        :return:
+        """
+
+        states = [self.__persist(group = group) for group in self.__data['group'].unique()]
 
         return all(states)
